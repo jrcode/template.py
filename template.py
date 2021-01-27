968eccd4d68b886f010477907fd8c98a9c1eecd0
Jose R Hernandez <2218865+jrcode@users.noreply.github.com>
2021-01-27 00:07:54 -0600

    initial commit

---
+++ template.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+__author__ = "Jose R. Hernandez"
+__email__ = "jr.hernandez4@gmail.com"
+__status__ = "Development"
+
+# libraries
+import argparse
+
+
+# first function
+def stuff():
+
+
+
+def main():
+    # parse
+    parser = argparse.ArgumentParser()
+
+    # file argument
+    parser.add_argument()
+
+    # variable to hold args
+    args = parser.parse_args()
+
+    # first function
+    stuff()
+
+if __name__ == "__main__":
+    main()
